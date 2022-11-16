2c6800359433836440ccac6d7d865aff84ced440
Florent-F-Dev <68734060+Florent-F-Dev@users.noreply.github.com>
2022-11-16 11:03:37 +0100

    Florent - fix

--- front/src/pages/Clubs.jsx
+++ front/src/pages/Clubs.jsx
@@ -4,6 +4,15 @@ import ClubList from "../components/clubCards/ClubList";
 const Clubs = () => {
     return (
         <div>
+            <div className={"home-club"}>
+                <div className={"home-sub-1"}><span>CLUB</span></div>
+                <div className={"home-sub-2"}></div>
+                <div className={"home-sub-3"}></div>
+                <div className={"home-sub-4"}></div>
+                <div className={"home-sub-5-club"}></div>
+                <div className={"home-sub-6"}></div>
+
+            </div>
             <div className="input-group p-5">
                 <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
                 <button type="button" class="btn btn-outline-primary">search</button>
